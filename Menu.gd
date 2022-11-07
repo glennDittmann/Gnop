007d8314ca5ff0d00841a6320d2b720b3b01e3cd
glennDittmann <glenn.dittmann@protonmail.com>
2022-11-07 15:27:30 +0100

    add gonp with tennis court

--- Menu.gd
+++ Menu.gd
@@ -1,11 +1,6 @@
 extends Control
 
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
-
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	$VBoxContainer/StartButton.grab_focus()
@@ -18,7 +13,8 @@ func _ready():
 
 func _on_StartButton_pressed():
 	print('Pressing start button')
-	get_tree().change_scene("res://Placeholder.tscn")
+	get_tree().change_scene("res://Gonp/Main.tscn")
+	#get_tree().change_scene("res://Placeholder.tscn")
 
 
 func _on_OptionsButton_pressed():
