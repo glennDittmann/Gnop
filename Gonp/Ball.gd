d5be0dc093d18a53678c9551f98708f77df51207
glennDittmann <glenn.dittmann@protonmail.com>
2022-11-08 22:48:54 +0100

    add bats and ball

---
+++ Gonp/Ball.gd
@@ -0,0 +1,17 @@
+extends KinematicBody2D
+
+var speed: int = 5
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	if(Input.is_action_pressed("move_up")):
+		position.y -= speed
+	elif(Input.is_action_pressed("move_down")):
+		position.y += speed
+	elif(Input.is_action_just_pressed("ui_cancel")):
+		get_tree().change_scene("res://Menu.tscn")
