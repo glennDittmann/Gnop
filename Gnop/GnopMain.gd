9430e96ae9aabac28542b1bf3e614f50e81d2b68
glennDittmann <glenn.dittmann@protonmail.com>
2022-11-14 23:27:37 +0100

    add end game -> main menu

--- Gnop/GnopMain.gd
+++ Gnop/GnopMain.gd
@@ -15,3 +15,9 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
+
+
+func _on_OutZone_body_entered(body: Node):
+	if body.is_in_group("ball"):
+		print("Game Over")
+		get_tree().change_scene("res://Menu.tscn")  # change for game over screen
