4401d9cb4f60a3ab6e6e4abc8c2859c2911f866c
Alexander Dittmann <alex.dittmann@gmail.com>
2022-11-18 17:59:11 +0100

    fix weird top and bottom collisions with bats
    done via replacing collisionshape type Rectangle with type Segment

--- Gnop/GnopMain.gd
+++ Gnop/GnopMain.gd
@@ -3,6 +3,11 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	# disable left collision segment for left bat and right segment for right bat
+	# otherwise it can get stuck between them
+	$BatLeft.get_node("LeftBorder").set_deferred("disabled", true)
+	$BatRight.get_node("RightBorder").set_deferred("disabled", true)
+	
 	GlobalVariables.points = 0
 	$AudioPlayer.play()
 
